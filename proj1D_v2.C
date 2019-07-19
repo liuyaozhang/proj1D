777f3ae8d4660c13fb8020ab96da31a4593934c1
Liuyao Zhang <lz55@bonner01.rice.edu>
2019-07-19 00:20:51 -0500

    fit option: least squares, h2DSignal_D0->Scale(1./nenties)

---
+++ proj1D_v2.C
@@ -0,0 +1,185 @@
+#include "include/myAnaConsts.h"
+
+void proj1D_v2(const char* input_d0= "/home/lz55/FlowCorr/proj1D/foutlist/HighMultiplicity/MultiplicityMT100LT250_absRapidityMT0LT2_Pt1.5_3.0_5.0_7.0_10.0/list.total_HM_new.root ", const char* input_ref = "/home/lz55/Code_v2_check/corr2D_trg_ref_PPHM80-inf_d0ana_ntrk.root ")
+{
+   gStyle->SetOptStat(11);
+   gStyle->SetOptFit(111);  
+
+   TFile* f1 = new TFile(input_d0);
+   TFile* f2 = new TFile(input_ref);
+   TH2D* h2DSignal_D0_Small[ana::nMass][ana::nPt];
+   TH2D* h2DBackground_D0_Small[ana::nMass][ana::nPt];
+   TH1D* hMult_raw_D0[ana::nMass][ana::nPt];
+   
+   TH1D* hMass_D0[ana::nMass][ana::nPt];
+   TH1D* hMass_pt_small[ana::nPt]; 
+
+   int Content_Background_Deta0_Dphi0[ana::nMass][ana::nPt] = {0};
+   int etaBin0 = 0;
+   int phiBin0 = 0;
+    
+   TF1 *constant = new TF1("constant","1.",-10,10);
+   double v2_smallDCA[ana::nMass][ana::nPt] = {0.};
+   double v2_smallDCA_err[ana::nMass][ana::nPt] = {0.};
+
+   double v2_smallDCA_Ref = {0.}; 
+   double v2_smallDCA_err_Ref = {0.}; 
+   
+   for(int ipt=0;ipt<ana::nPt;ipt++){
+      hMass_pt_small[ipt] = (TH1D*) f1->Get(Form("h1DMass_pt%d",ipt)); 
+      }
+
+   for(int imass=0; imass<ana::nMass; imass++){
+      for(int ipt=0; ipt<ana::nPt; ipt++){
+         h2DSignal_D0_Small[imass][ipt] = (TH2D*) f1->Get(Form("hSignal_mass%d_pt%d_smallDCA", imass, ipt));
+         h2DBackground_D0_Small[imass][ipt] = (TH2D*) f1->Get(Form("hBackground_mass%d_pt%d_smallDCA", imass, ipt));
+         hMass_D0[imass][ipt] = (TH1D*) f1->Get(Form("hMassD0_mass%d_pt%d", imass, ipt));
+         hMult_raw_D0[imass][ipt] = (TH1D*) f1->Get(Form("hMult_raw_D0_mass%d_pt%d", imass, ipt));
+         }
+     }
+
+   TH1D* h1DSignal_D0_Neg[ana::nMass][ana::nPt];
+   TH1D* h1DSignal_D0_Pos[ana::nMass][ana::nPt];
+ 
+   TH1D* h1DBackground_D0_Neg[ana::nMass][ana::nPt];
+   TH1D* h1DBackground_D0_Pos[ana::nMass][ana::nPt];
+   
+   TH1D* hDeltaPhi[ana::nMass][ana::nPt];
+   
+   Double_t etaCut[4] = {-4.95,-2.,2.,4.95};
+   Int_t etaBin[4]; 
+   for(int ie=0; ie<4; ie++) etaBin[ie] = h2DSignal_D0_Small[0][0]->GetXaxis()->FindBin(etaCut[ie]);
+   for(int imass=0; imass<ana::nMass; imass++){
+      for(int ipt=0; ipt<ana::nPt; ipt++){
+         
+         double nMult_D0= hMult_raw_D0[imass][ipt]->Integral(2, 251);
+         h2DSignal_D0_Small[imass][ipt]->Scale(1./nMult_D0); 
+
+         h1DSignal_D0_Neg[imass][ipt] = h2DSignal_D0_Small[imass][ipt]->ProjectionY(
+               Form("h1DSignal_D0_Neg_mass%d_pt%d", imass, ipt), etaBin[0],etaBin[1]-1);
+         h1DSignal_D0_Pos[imass][ipt] = h2DSignal_D0_Small[imass][ipt]->ProjectionY(
+               Form("h1DSignal_D0_Pos_mass%d_pt%d", imass, ipt), etaBin[2]+1,etaBin[3]+1);
+         h1DSignal_D0_Neg[imass][ipt]->Add(h1DSignal_D0_Pos[imass][ipt]); 
+         
+         h1DBackground_D0_Neg[imass][ipt] = h2DBackground_D0_Small[imass][ipt]->ProjectionY(
+               Form("h1DBackground_D0_Neg_mass%d_pt%d", imass, ipt), etaBin[0],etaBin[1]-1);
+         h1DBackground_D0_Pos[imass][ipt] = h2DBackground_D0_Small[imass][ipt]->ProjectionY(
+               Form("h1DBackground_D0_pos_mass%d_pt%d", imass, ipt), etaBin[2]+1,etaBin[3]+1);
+         h1DBackground_D0_Neg[imass][ipt]->Add(h1DBackground_D0_Pos[imass][ipt]);
+
+         int center = h2DBackground_D0_Small[imass][ipt]->FindBin(0., 0);
+         //cout << "center" << "=" << center<< endl; 
+         h1DSignal_D0_Neg[imass][ipt]->Divide(h1DBackground_D0_Neg[imass][ipt]); 
+         h1DSignal_D0_Neg[imass][ipt]->Scale(h2DBackground_D0_Small[ipt][imass]->GetBinContent(center)); 
+     }  
+   }
+   
+   TH2D* h2DSignal_D0_Ref;
+   TH2D* h2DBackground_D0_Ref; 
+   TH1D* hMult_Ref; 
+   TH1D* h1DSignal_D0_Neg_Ref;
+   TH1D* h1DSignal_D0_Pos_Ref;
+   
+   TH1D* h1DBackground_D0_Neg_Ref; 
+   TH1D* h1DBackground_D0_Pos_Ref; 
+   
+   cout << "test1++++++++++++++++"<< endl; 
+   h2DSignal_D0_Ref = (TH2D*) f2->Get("hSignal_Ref_trk1");
+   h2DBackground_D0_Ref = (TH2D*) f2->Get("hBackground_Ref_trk1" );
+   hMult_Ref = (TH1D*) f2->Get("hMult_trk1");
+   int nMult_Ref = hMult_Ref->Integral(2,256); 
+
+   int etaBin_Ref = h2DBackground_D0_Ref->GetXaxis()->FindBin(0.);
+   int phiBin_Ref = h2DBackground_D0_Ref->GetYaxis()->FindBin(0.);
+   int Content_Background_Deta0_Dphi0_Ref = h2DBackground_D0_Ref->GetBinContent(etaBin_Ref, phiBin_Ref);
+   
+   h1DSignal_D0_Neg_Ref = h2DSignal_D0_Ref->ProjectionY("h1DSignal_D0_Neg_Ref", etaBin[0],etaBin[1]-1);
+   h1DSignal_D0_Pos_Ref = h2DSignal_D0_Ref->ProjectionY("h1DSignal_D0_Pos_Ref", etaBin[2]+1,etaBin[3]+1);
+   h1DSignal_D0_Neg_Ref->Add(h1DSignal_D0_Pos_Ref);
+
+   h1DBackground_D0_Neg_Ref = h2DBackground_D0_Ref->ProjectionY("h1DBackground_D0_Neg_Ref", etaBin[0],etaBin[1]-1);
+   h1DBackground_D0_Pos_Ref = h2DBackground_D0_Ref->ProjectionY("h1DBackground_D0_Neg_Ref", etaBin[2]+1,etaBin[3]+1);
+   h1DBackground_D0_Neg_Ref->Add(h1DBackground_D0_Pos_Ref);
+   
+   h1DSignal_D0_Neg_Ref->Divide(h1DBackground_D0_Neg_Ref); 
+   h1DSignal_D0_Neg_Ref->Scale(Content_Background_Deta0_Dphi0_Ref); 
+   cout << "test2++++++++++++" << endl; 
+   
+   //Fitting 
+   std::string function = "[0]*(1+2*([1]*TMath::Cos(x)+[2]*TMath::Cos(2*x)+[3]*TMath::Cos(3*x)))";
+
+   TCanvas* cMass[14][4];
+   char Tfname[128]; 
+   char Tfname1[128]; 
+   for(int imass=0; imass<ana::nMass; imass++){
+      for(int ipt=0; ipt<ana::nPt; ipt++){
+         cMass[imass][ipt] = new TCanvas();
+         TF1 func("deltaPhi", function.c_str(), -3.14159*0.5, 3.14159*1.5);
+         func.SetParameter(0, h1DSignal_D0_Neg[imass][ipt]->GetMaximum());
+         func.SetParameter(1, 0.1);
+         func.SetParameter(2, 0.1);
+         func.SetParameter(3, 0.1);
+
+         h1DSignal_D0_Neg[imass][ipt]->Fit(&func, "q");
+         h1DSignal_D0_Neg[imass][ipt]->Fit(&func, "q");
+         h1DSignal_D0_Neg[imass][ipt]->Fit(&func, "m q");
+         h1DSignal_D0_Neg[imass][ipt]->Fit(&func, "m q E");
+        
+         v2_smallDCA[imass][ipt] = func.GetParameter(2);
+         v2_smallDCA_err[imass][ipt] = func.GetParError(2);
+            
+         sprintf(Tfname, "./out_check/pro_h1DSignal_D0_Neg_%d_%d.png",imass,ipt);
+         cMass[imass][ipt]->SaveAs(Tfname); 
+         delete cMass[imass][ipt]; 
+      }
+   } 
+   
+   TCanvas* cRef; 
+
+   cout << "test3++++++++++" << endl; 
+   cRef = new TCanvas();
+   TF1 func_ref("deltaPhi_Ref", function.c_str(), -3.14159*0.5, 3.14159*1.5);
+   func_ref.SetParameter(0, h1DSignal_D0_Neg_Ref->GetMaximum());
+   func_ref.SetParameter(1, 0.1);
+   func_ref.SetParameter(2, 0.1);
+   func_ref.SetParameter(3, 0.1);
+
+   h1DSignal_D0_Neg_Ref->Fit(&func_ref, "e q");
+   h1DSignal_D0_Neg_Ref->Fit(&func_ref, "e q");
+   h1DSignal_D0_Neg_Ref->Fit(&func_ref, "e");
+   
+   v2_smallDCA_Ref = func_ref.GetParameter(2); 
+   v2_smallDCA_err_Ref = func_ref.GetParError(2); 
+
+   sprintf(Tfname1, "./out_check/pro_h1DSignal_D0_Neg_Ref.png");
+   cRef->SaveAs(Tfname1); 
+   delete cRef; 
+
+   //vn(p_{T}^{trig})
+   TGraphErrors* v2_mass_pt_smallDCA[ana::nPt];
+   for(int ipt=0; ipt<ana::nPt; ipt++){
+      v2_mass_pt_smallDCA[ipt] = new TGraphErrors(ana::nMass);
+      v2_mass_pt_smallDCA[ipt]->SetName(Form("v2_mass_pt_smallDCA_pt%d", ipt));
+
+      for(int imass=0; imass<ana::nMass; imass++){
+         double temp = v2_smallDCA[imass][ipt]/sqrt(v2_smallDCA_Ref);
+         double temp_err = temp* sqrt(pow(v2_smallDCA_err[imass][ipt]/v2_smallDCA[imass][ipt], 2)
+               + pow(0.5*v2_smallDCA_err_Ref/v2_smallDCA_Ref, 2));
+
+         v2_smallDCA[imass][ipt] = temp;
+         v2_smallDCA_err[imass][ipt] = temp_err;
+
+         v2_mass_pt_smallDCA[ipt]->SetPoint(imass, hMass_D0[imass][ipt]->GetMean(), v2_smallDCA[imass][ipt]);
+         v2_mass_pt_smallDCA[ipt]->SetPointError(imass, 0, v2_smallDCA_err[imass][ipt]);
+      }
+   }
+
+   TFile f3(Form("out_check/%s_v2.root", "D0_update"), "recreate");
+
+   for(int ipt=0; ipt<ana::nPt; ipt++){
+      v2_mass_pt_smallDCA[ipt]->Write();
+      hMass_pt_small[ipt]->Write();
+   }
+
+   return;
+}
